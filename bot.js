320597c7831f6f42f67b7306e569228ca14970e2
nathanishandsome <59988934+nathanishandsome@users.noreply.github.com>
2020-01-17 14:58:21 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,8 @@
-const Discord = require('discord.js');
+const discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.DISCORD_BOT_SECRET;
+
+client.on('ready', () => {
+  console.log("I'm in");
+  console.log(client.user.username);
+});
